0dfd2e622182f3a71277afcf344a1dc073f05028
MarcosScheuermann <scheuermannjm@gmail.com>
2021-12-25 18:00:48 -0300

    guardar-LS

--- js/note.js
+++ js/note.js
@@ -1,1 +1,20 @@
-const note = note;
+// let nota = [];
+// let notaLS = JSON.stringify(nota);
+// localStorage.setItem("nota", notaLS);
+
+//para guardar en LS
+
+let nota = JSON.parse(localStorage.getItem("nota"));
+if (!nota) {
+  nota = [];
+  localStorage.setItem("nota", JSON.stringify(nota));
+}
+
+nota.forEach((textonota) => {
+  const nota = document.createElement("div");
+  nota.innerText = textonota;
+  nota.classList.add("note-style", "m-3");
+  const notaContainer = document.querySelector(".tablero");
+  notaContainer.appendChild(nota);
+});
+
